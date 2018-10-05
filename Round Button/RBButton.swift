adbaf82e96cea0dd02e5019f3b9103db07d119f4
Gabriel Mesquita <gmesquit@gmail.com>
2018-10-05 10:43:18 -0600

    initial commit

---
+++ Round Button/RBButton.swift
@@ -0,0 +1,24 @@
+//
+//  RBButton.swift
+//  Round Button
+//
+//  Created by Gabriel Mesquita on 2018-10-05.
+//  Copyright © 2018 Instantiate inc. All rights reserved.
+//
+
+
+// MARK:- Frameworks
+import UIKit
+
+
+// MARK:- RBButton class
+class RBButton: UIButton {
+    
+    
+    // MARK:- Methods
+    // MARK:- Method 3
+    // Subclass UIButton and change the corner radius in the draw method
+    override func draw(_ rect: CGRect) {
+        
+    }
+}
--- Round Button/ViewController.swift
+++ Round Button/ViewController.swift
@@ -6,15 +6,41 @@
 //  Copyright © 2018 Instantiate inc. All rights reserved.
 //
 
+
+// MARK:- Frameworks
 import UIKit
 
-class ViewController: UIViewController {
 
+// MARK:- ViewController class
+class ViewController: UIViewController {
+    
+    
+    // MARK:- Outlets
+    // MARK:- Method 0
+    // Change the corner radius when the outlet is set using a property observer
+    @IBOutlet weak var button0: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    // MARK:- Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // MARK:- Method 1
+        // Change the corner radius when the view loads into memory
+        
     }
-
-
 }
 
+
+// MARK:- UIButton extension
+extension UIButton {
+    
+    
+    // MARK:- Method 2
+    // Extend UIButton and implement a method to make itself circular
+    
+}
